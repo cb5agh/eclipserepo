04735949bd396cf9fba5c79a22e13ebb4fca2201
cb5agh <ghoffar.alvi@googlemail.com>
2019-01-22 02:18:41 +0500

    Eclipse Test Project

---
+++ testproj1/src/testproj1/helloworld.java
@@ -0,0 +1,12 @@
+package testproj1;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ....");
+		System.out.print("test1");
+
+	}
+
+}
